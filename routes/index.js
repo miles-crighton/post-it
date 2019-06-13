65562a938952f5f841e310d8713166c0d04f7f21
Miles Crighton <miles.crighton@gmail.com>
2019-06-13 17:03:38 +0100

    Persistent post functionality added server-side

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-var data = [5,6,7,8]
+var data = [
+  {
+    text: 'Hello, world!',
+    author: 'Miles',
+    date: '13-06-19',
+    time: '15:23',
+    posX: 100,
+    posY: 100
+  }
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', data: data });
+  res.render('index', { title: 'Post-it!', data: data });
 });
 
 router.post('/add', function (req, res, next) {
   // Prepare output in JSON format
-  console.log(req.body.message)
-  data.push(req.body.message)
+  console.log(req.body)
+  data.push(req.body)
   response = {
     data: data,
   };
-
   console.log(response);
-  //res.end(JSON.stringify(response));
+  res.end(JSON.stringify(response));
 });
+
+router.get('/data', function(req, res, next) {
+  response = { data: data }
+  res.end(JSON.stringify(response));
+});
+
+function generateMessage(message) {
+}
 
 module.exports = router;
